dc373f500c354359e24adee8685931f7fc430ab0
gee2789 <geeyung.li@gmail.com>
2017-03-27 14:47:02 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    binding.pry
+    session.destroy
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/sessions', to: 'sessions#create', as: 'sessions'
-
+  delete '/sessions', to: 'sessions#destroy', as: 'logout'
 end
